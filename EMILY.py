751948fb7e22f1c81861e6482f4633c2fd685ea0
Gustavo <37788848+nGustavin@users.noreply.github.com>
2020-11-05 17:11:35 -0300

    Update EMILY.py

--- EMILY.py
+++ EMILY.py
@@ -2,16 +2,12 @@ import requests
 import json
 from lxml import html
 
-proxies = {
-    'http': 'http://estag-nii:pantanal2020@proxypge01.pge.parana:8080',
-    'https': 'http://estag-nii:pantanal2020@proxypge01.pge.parana:8080',
-}
 
-url = 'https://www.gsmcelulares.com.br/'
+url = 'https://www.google.com'
 counter = 1
 
 while True:
-    req = requests.get(url, proxies=proxies)
+    req = requests.get(url)
     body = req.text
     counter += 1
-    print(counter, "requisicoes")
+    print(counter, "requests")
